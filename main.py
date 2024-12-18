dbb290305aa0ef463379c782f0e0ca2ed0888359
np362 <np8643@mci4me.at>
2024-12-18 14:48:29 +0100

    ui_device into main with tabs test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from queries import find_devices
+from devices import Device
 from users import User
 
 tab1, tab2, tab3 = st.tabs(["Geräte", "Nutzer", "Reservierung"])
@@ -11,21 +13,39 @@ if "sb_current_device" not in st.session_state:
 
 # Tabs
 with tab1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+   st.header("Geräteauswahl")
 
-   # Eine Überschrift der ersten Ebene
-   st.write("# Gerätemanagement")
+   devices_in_db = find_devices()
 
-    # Eine Überschrift der zweiten Ebene
-   st.write("## Geräteauswahl")
+   if devices_in_db:
+       current_device_name = st.selectbox(
+           'Gerät auswählen',
+           options=devices_in_db, key="sbDevice")
 
-    # Eine Auswahlbox mit hard-gecoded Optionen, das Ergebnis
+       if current_device_name in devices_in_db:
+           loaded_device = Device.find_by_attribute("device_name", current_device_name)
+           if loaded_device:
+               st.write(f"Loaded Device: {loaded_device}")
+           else:
+               st.error("Device not found in the database.")
 
-   st.session_state.sb_current_device = st.selectbox(label='Gerät auswählen',
-            options = ["Gerät_A", "Gerät_B"])
+           with st.form("Device"):
+               st.write(loaded_device.device_name)
 
-   st.write(F"Das ausgewählte Gerät ist {st.session_state.sb_current_device}")
+               text_input_val = st.text_input("Geräte-Verantwortlicher", value=loaded_device.managed_by_user_id)
+               loaded_device.set_managed_by_user_id(text_input_val)
+
+                # Every form must have a submit button.
+               submitted = st.form_submit_button("Submit")
+               if submitted:
+                   loaded_device.store_data()
+                   st.write("Data stored.")
+                   st.rerun()
+       else:
+           st.error("Selected device is not in the database.")
+   else:
+       st.write("No devices found.")
+       st.stop()
 
 with tab2:
    st.header("A dog")
